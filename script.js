99a3b72ad92fbb1757f2f2d9652c9d3b013823f9
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2015-07-21 17:07:35 +0200

    layout almost completed

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+var progress = 0;
+var progressBarBG = 0;
+var progressBarLabel = 0;
+
+function fillProgressBar()
+{
+    if(progress >= 100)
+        return;
+
+    ++progress;
+    progressBarBG.css("width", progress+"%");
+
+    progressBarLabel.text(progress+"%");
+
+    window.setTimeout(fillProgressBar, 10);
+}
+
 function init()
 {
+    progressBarBG = $("#progressBarBackground");
+    progressBarLabel = $("#progressBarLabel");
 
+    $("#workButton").click(fillProgressBar);
 }
 
 window.onload = init;
