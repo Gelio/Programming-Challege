bbb04692fd1f1e920d7dc2c6b3f6588142732fde
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2015-07-21 23:03:30 +0200

    changed layout, added sounds and basic clicking

--- script.js
+++ script.js
@@ -1,26 +1,105 @@
-var progress = 0;
-var progressBarBG = 0;
-var progressBarLabel = 0;
+var basePower = 1;
+var smallProjectTitles = ["Project 1", "Project 2", "Project 3"];
+var soundsEnabled = true;
 
-function fillProgressBar()
+
+
+var cash = 0;
+
+
+var smallProjectTitlesAmount = smallProjectTitles.length;
+var Player = {
+    power: basePower
+};
+
+var SmallProject = {
+    title: "Title",
+    currAmount: 0,
+    totalAmount: 0,
+    reward: 0,
+
+    updateTitle: function() {
+        $("#smallProjectTitle").text(this.title);
+    },
+
+    updateAmounts: function() {
+        var amounts = $("#smallProjectProgressAmount");
+        amounts.children("span.currentAmount").html(this.currAmount);
+        amounts.children("span.totalAmount").html(this.totalAmount);
+
+        this.updateProgressBar();
+    },
+
+    updateProgressBar: function() {
+        var percentage = 100;
+        if(this.totalAmount != 0)
+            percentage = Math.floor(this.currAmount/this.totalAmount*100);
+
+        $("#smallProjectProgressBarBackground").css("width", percentage+"%");
+        $("#smallProjectProgressBarLabel").text(percentage+"%");
+    },
+
+    updateReward: function() {
+        $("#smallProjectRewardAmount").text(this.reward);
+    },
+
+    update: function() {
+        this.updateTitle();
+        this.updateAmounts();
+        this.updateReward();
+    },
+
+    shuffle: function() {
+        this.title = smallProjectTitles[Math.floor(Math.random()*smallProjectTitlesAmount)];
+        this.currAmount = 0;
+        this.totalAmount = Math.floor(Math.random()*8+2);
+        this.reward = Math.floor(Math.random()*10+1);
+        this.update();
+    },
+
+    addPoints: function(points) {
+        this.currAmount += points;
+        if(this.currAmount > this.totalAmount)
+            this.currAmount = this.totalAmount;
+
+        // Check if finished project
+        if(this.currAmount == this.totalAmount)
+        {
+            // Project is finished
+            cash += this.reward;
+            updateCash();
+            this.shuffle();
+            
+            if(soundsEnabled)
+                document.getElementById("cash_in").play();
+        }
+        this.updateAmounts();
+    }
+};
+
+function updateCash()
 {
-    if(progress >= 100)
-        return;
+    $("#cash").text("$"+cash);
+}
 
-    ++progress;
-    progressBarBG.css("width", progress+"%");
-
-    progressBarLabel.text(progress+"%");
-
-    window.setTimeout(fillProgressBar, 10);
+function smallProjectButtonClick()
+{
+    SmallProject.addPoints(Player.power);
 }
 
 function init()
 {
-    progressBarBG = $("#progressBarBackground");
-    progressBarLabel = $("#progressBarLabel");
 
-    $("#workButton").click(fillProgressBar);
+    var upgrades = $(".upgrade");
+    upgrades.mouseover(function() { $(this).children(".name").addClass("bold");});
+    upgrades.mouseout(function() { $(this).children(".name").removeClass("bold");});
+
+    $("#smallProjectReward").css("line-height", $("#smallProjectProgressBar").outerHeight()+"px");
+
+    $("#smallProjectWorkButton").click(smallProjectButtonClick);
+
+    updateCash();
+    SmallProject.shuffle();
 }
 
 window.onload = init;
