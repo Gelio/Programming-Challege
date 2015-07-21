e9e2ea8b51ef2d7cdf8ed771f4584436308dbc9b
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2015-07-22 00:03:00 +0200

    added full upgrades support

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+/*
+Author: Grzegorz Rozdzialik <voreny.gelio@gmail.com>
+Configuration
+ */
 var basePower = 1;
 var smallProjectTitles = ["Project 1", "Project 2", "Project 3"];
+var upgradesList = [
+    {
+        id: "upgrade-training",
+        name: "Training",
+        cost: 5,
+        costMultiplier: 1.3, // amount by which cost is multiplied with each upgrade
+        clickGain: 2,
+        passiveGain: 0
+    },
+    {
+        id: "upgrade-course",
+        name: "Programming course",
+        cost: 10,
+        costMultiplier: 1.3,
+        clickGain: 4,
+        passiveGain: 0
+    }
+];
 var soundsEnabled = true;
 
 
@@ -8,6 +30,92 @@ var cash = 0;
 
 
 var smallProjectTitlesAmount = smallProjectTitles.length;
+var upgradesAmount = upgradesList.length;
+
+var Upgrades = {
+    build: function() {
+        for(var i=0; i < upgradesAmount; ++i)
+        {
+            var upgrade = document.createElement("div");
+            $(upgrade).addClass("upgrade");
+            $(upgrade).attr("id", upgradesList[i].id);
+
+            var cost = document.createElement("div");
+            $(cost).addClass("cost");
+            $(cost).text("$"+upgradesList[i].cost);
+
+            var name = document.createElement("div");
+            $(name).addClass("name");
+            $(name).text(upgradesList[i].name);
+
+            var effect = document.createElement("div");
+            $(effect).addClass("effect");
+            if(upgradesList[i].clickGain > 0)
+                $(effect).text("+" + upgradesList[i].clickGain + " pts/click");
+
+            if(upgradesList[i].passiveGain > 0)
+                $(effect).text("+" + upgradesList[i].passiveGain + " pts/sec");
+
+            $(upgrade).append(cost);
+            $(upgrade).append(name);
+            $(upgrade).append(effect);
+            $(upgrade).click(this.clickUpgrade);
+            $("#upgradesList").append(upgrade);
+        }
+    },
+
+    updateUpgrade: function(upgradeNumber) {
+        var upgrade = document.getElementById(upgradesList[upgradeNumber].id), upgradeChildren = upgrade.childNodes;
+
+        console.log(upgradesList[upgradeNumber].cost);
+        // Cost
+        $(upgradeChildren[0]).text("$" + upgradesList[upgradeNumber].cost);
+
+        // Name
+        $(upgradeChildren[1]).text(upgradesList[upgradeNumber].name);
+
+        //Effect
+        if(upgradesList[upgradeNumber].clickGain > 0)
+            $(upgradeChildren[2]).text("+" + upgradesList[upgradeNumber].clickGain + " pts/click");
+
+        if(upgradesList[upgradeNumber].passiveGain > 0)
+            $(upgradeChildren[2]).text("+" + upgradesList[upgradeNumber].passiveGain + " pts/sec");
+    },
+
+    updateAll: function() {
+        for(var i=0; i < upgradesAmount; ++i)
+            this.updateUpgrade(upgradesList[i].id);
+    },
+
+    clickUpgrade: function() {
+        var upgradeNumber = 0;
+        for(var i=0; i < upgradesAmount; ++i)
+        {
+            if(upgradesList[i].id == $(this).attr("id"))
+            {
+                upgradeNumber = i;
+                break;
+            }
+        }
+
+        if(cash >= upgradesList[upgradeNumber].cost)
+        {
+            // Can afford
+            Player.power += upgradesList[upgradeNumber].clickGain;
+
+            cash -= upgradesList[upgradeNumber].cost;
+            updateCash();
+
+            upgradesList[upgradeNumber].cost = Math.round(upgradesList[upgradeNumber].cost * upgradesList[upgradeNumber].costMultiplier);
+            Upgrades.updateUpgrade(upgradeNumber);
+        }
+        else
+        {
+            // Insufficient funds
+        }
+    }
+};
+
 var Player = {
     power: basePower
 };
@@ -52,8 +160,8 @@ var SmallProject = {
     shuffle: function() {
         this.title = smallProjectTitles[Math.floor(Math.random()*smallProjectTitlesAmount)];
         this.currAmount = 0;
-        this.totalAmount = Math.floor(Math.random()*8+2);
-        this.reward = Math.floor(Math.random()*10+1);
+        this.totalAmount = randomInteger(5,15);
+        this.reward = randomInteger(5, 15);
         this.update();
     },
 
@@ -69,11 +177,15 @@ var SmallProject = {
             cash += this.reward;
             updateCash();
             this.shuffle();
-            
+
             if(soundsEnabled)
                 document.getElementById("cash_in").play();
         }
         this.updateAmounts();
+    },
+
+    buttonClick: function() {
+        SmallProject.addPoints(Player.power);
     }
 };
 
@@ -82,13 +194,14 @@ function updateCash()
     $("#cash").text("$"+cash);
 }
 
-function smallProjectButtonClick()
+function randomInteger(min, max)
 {
-    SmallProject.addPoints(Player.power);
+    return Math.floor(Math.random()*(max-min)+min);
 }
 
 function init()
 {
+    Upgrades.build();
 
     var upgrades = $(".upgrade");
     upgrades.mouseover(function() { $(this).children(".name").addClass("bold");});
@@ -96,7 +209,7 @@ function init()
 
     $("#smallProjectReward").css("line-height", $("#smallProjectProgressBar").outerHeight()+"px");
 
-    $("#smallProjectWorkButton").click(smallProjectButtonClick);
+    $("#smallProjectWorkButton").click(SmallProject.buttonClick);
 
     updateCash();
     SmallProject.shuffle();
